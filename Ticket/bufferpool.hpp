35b9dd4f1c4ff140608d2c997dceb152478e2f85
Xinyu Mao <45277532+sparkmxy@users.noreply.github.com>
2019-06-02 10:35:09 +0800

    3000 bufferpool pages

--- Ticket/bufferpool.hpp
+++ Ticket/bufferpool.hpp
@@ -62,7 +62,7 @@ struct to_block_t{
     }
 };
 #ifndef BUF_POOL_TOTAL_NUM
-    #define BUF_POOL_TOTAL_NUM 1100
+    #define BUF_POOL_TOTAL_NUM 3000
 #endif
 #ifndef BUF_COLD_PERCENTAGE
     #define BUF_COLD_PERCENTAGE 12
